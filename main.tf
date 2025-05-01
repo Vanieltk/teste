73641a5e83cc6ae1c486ecfd58c2bed042520f98
Vaniel Timm Koschier <73503310+Vanieltk@users.noreply.github.com>
2025-05-01 20:11:00 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "web" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+
+  root_block_device {
+    volume_size = 10    
+    volume_type = "gp3"    
+  }
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Permite SSH e HTTP"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
